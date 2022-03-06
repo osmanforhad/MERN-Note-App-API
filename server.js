977da83a5af18195aee7eba0cfc337684f9cbdf4
osmanforhad <osmanforhad93@gmail.com>
2022-03-06 13:54:55 +0600

    setup node server for this api

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//__Create Express App__//
+const app = express();
+
+//__Parse requests of content-type - application/x-ww-form-urlencoded__//
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//__Parse requests of content-type - application/json__//
+app.use(bodyParser.json());
+
+//__define a simple root route__//
+app.get("/", (request, response) => {
+  response.json({
+    message:
+      "Welcome to easy note taking application. Takes note quickly. Organize and keep track af all your notes.",
+  });
+
+  //__Setup Request Listener__//
+  app.listen(5000, () => {
+    console.log("Server is Running on port: 5000");
+  });
+});
