92fe836aa6fd7be2191e74bf7f8413d5ba00ab04
osmanforhad <osmanforhad93@gmail.com>
2022-03-06 18:01:21 +0600

    write method for create a new note and save into database

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -2,7 +2,28 @@ const NoteModel = require("../models/note.model");
 
 //__Method for Create and Save a New Note__//
 exports.create = (request, response) => {
-  console.log("Method for Create New Note");
+  //Validate Request
+  if (!request.body.content) {
+    return request.response.status(400).send({
+      message: "Note content can't be empty",
+    });
+  }
+  //Create a Note
+  const note = new NoteModel({
+    title: request.body.title || "Untitled Notes",
+    content: request.body.content,
+  });
+  //Save Note into the Database
+  note
+    .save()
+    .then((data) => {
+      response.send(data);
+    })
+    .catch((error) => {
+      response.status(500).send({
+        message: error.message || "Some error occurred while creating a note",
+      });
+    });
 };
 
 //__Retrieve and return all notes from the Database__//
